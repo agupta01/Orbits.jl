60f370f077a31c69ce635f17e3986de799d3d529
Arunav Gupta <centaurus.ab@gmail.com>
2023-02-16 07:39:08 -0800

    feat: orbit propagation and tof

--- src/Orbits.jl
+++ src/Orbits.jl
@@ -389,7 +389,268 @@ function gibbs(R1::Vector{Float64}, R2::Vector{Float64}, R3::Vector{Float64}, ge
     end
 end
 
+"""
+Determines a position vector given a single observation from a specified ground site.
 
-export Earth, getapses, RV2koe, koe2rv, RV2coe, coe2RV, makeR, gibbs
+# Arguments
+- `ϕ`: latitude of the ground site, in degrees
+- `λ`: longitude of the ground site, in degrees
+- `ρ`: range from the ground site to the satellite, in km
+- `β`: azimuth angle from the ground site to the satellite, in degrees
+- `σ`: elevation angle from the ground site to the satellite, in degrees
+- `angletype`: "deg" or "rad", depending on the units of the angles
+
+# Returns
+- `R`: position vector of the satellite in ECI coordinates, in km
+"""
+function singleangle(ϕ::Float64, λ::Float64, ρ::Float64, β::Float64, σ::Float64, angletype::String)::Vector{Float64}
+    # Convert to radians if necessary
+    if angletype == "deg"
+        ϕ = ϕ * (π / 180)
+        λ = λ * (π / 180)
+        β = β * (π / 180)
+        σ = σ * (π / 180)
+    end
+
+    I = [1.0, 0.0, 0.0]
+    J = [0.0, 1.0, 0.0]
+    K = [0.0, 0.0, 1.0]
+
+    # Compute ρ_SEZ (range vector in SEZ coordinates)
+    ρ_SEZ = ρ * (-cos(β) * cos(σ) * I + sin(β) * cos(σ) * J + sin(σ) * K)
+
+    # Compute the transformation matrix
+    sλ = sin(λ)
+    cλ = cos(λ)
+    sϕ = sin(ϕ)
+    cϕ = cos(ϕ)
+    D = zeros(3, 3)
+    D[1, 1] = sϕ * cλ
+    D[1, 2] = -sλ
+    D[1, 3] = cϕ * cλ
+    D[2, 1] = sϕ * sλ
+    D[2, 2] = cλ
+    D[2, 3] = cϕ * sλ
+    D[3, 1] = -cϕ
+    D[3, 3] = sϕ
+
+    # Compute the range vector in ECI coordinates
+    ρ_ECI = D * ρ_SEZ
+
+    # Get the site vector
+    R_site = Earth.r * (cϕ * cλ * I + cϕ * sλ * J + sϕ * K)
+
+    return R_site + ρ_ECI
+end
+
+"""
+Convert true anomaly to eccentric anomaly.
+
+# Arguments
+- `e`: eccentricity
+- `θ`: true anomaly, in degrees
+- `angletype`: "deg" or "rad", depending on the units of the angles
+
+# Returns
+- `E`: eccentric anomaly, in degrees
+"""
+function θ2E(e::Float64, θ::Float64, angletype::String)::Float64
+    # Convert to radians if necessary
+    if angletype == "deg"
+        θ = θ * (π / 180)
+    end
+
+    # Compute the eccentric anomaly
+    E = 2 * atan(√((1 - e) / (1 + e)) * tan(θ / 2))
+
+    # Convert to degrees if necessary
+    if angletype == "deg"
+        E = E * (180 / π)
+    end
+
+    return E
+end
+
+"""
+Convert eccentric anomaly to true anomaly.
+
+# Arguments
+- `e`: eccentricity
+- `E`: eccentric anomaly, in degrees
+- `angletype`: "deg" or "rad", depending on the units of the angles
+
+# Returns
+- `θ`: true anomaly, in degrees
+"""
+function E2θ(e::Float64, E::Float64, angletype::String)::Float64
+    # Convert to radians if necessary
+    if angletype == "deg"
+        E = E * (π / 180)
+    end
+
+    # Compute the true anomaly
+    θ = 2 * atan(√((1 + e) / (1 - e)) * tan(E / 2))
+
+    # Convert to degrees if necessary
+    if angletype == "deg"
+        θ = θ * (180 / π)
+    end
+
+    return θ
+end
+
+"""
+Determine time of flight given a (position, velocity) state and a true anomaly change.
+
+# Arguments
+- `R`: position vector, in km at t₀
+- `V`: velocity vector, in km/s at t₀
+- `Δθ`: change in true anomaly, in degrees
+
+# Returns
+- `Δt`: time of flight, in seconds
+"""
+function TOF(R::Vector{Float64}, V::Vector{Float64}, Δθ::Float64)::Float64
+    # Compute the orbital elements at t₀
+    a, e, i, Ω, ω, θ = RV2coe(R, V, "deg")
+
+    # Compute the eccentric anomalies at t₀ and t₀ + Δt
+    E0 = θ2E(e, θ, "deg")
+    E1 = θ2E(e, θ + Δθ, "deg")
+
+    # Fix E0 or E1 if they are negative
+    if E0 < 0
+        E0 += 360
+    end
+    if E1 < 0
+        E1 += 360
+    end
+
+    # Convert to radians
+    E0 = E0 * (π / 180)
+    E1 = E1 * (π / 180)
+
+    # Compute the mean motion
+    n = √(Earth.μ / a^3)
+
+    # Use Kepler's equation to get TOF
+    Δt = (1 / n) * (E1 - e * sin(E1) - E0 + e * sin(E0))
+
+    # If Δt is negative, add 2π/n
+    if Δt < 0
+        Δt += 2π / n
+    end
+
+    return Δt
+end
+
+
+"""
+Propagate an orbit using Lagrangian coefficients.
+
+# Arguments
+- `R`: position vector, in km at t₀
+- `V`: velocity vector, in km/s at t₀
+- `Δθ`: change in true anomaly, in degrees
+
+# Returns
+- `R1`: position vector, in km at t₀ + Δt
+- `V1`: velocity vector, in km/s at t₀ + Δt
+"""
+function propagate(R::Vector{Float64}, V::Vector{Float64}, Δθ::Float64)::Tuple{Vector{Float64},Vector{Float64}}
+    # Compute the necessary orbital elements at t₀
+    h = R × V
+    p = norm(h)^2 / Earth.μ
+    ξ = norm(V)^2 / 2 - Earth.μ / norm(R)
+    a = -Earth.μ / (2 * ξ)
+    e = √(1 - p / a)
+
+    # Convert Δθ to radians
+    Δθ = Δθ * (π / 180)
+
+    # Determine initial and final true anomaly, in radians
+    θ0 = acos((p / norm(R) - 1) / e)
+    θ1 = θ0 + Δθ
+
+    # Compute future r
+    r0 = norm(R)
+    r = p / (1 + e * cos(θ1))
+
+    # Compute the Lagrangian coefficients
+    f = 1 - (r / p) * (1 - cos(Δθ))
+    g = (r * r0 * sin(Δθ)) / (√(p * Earth.μ))
+    fdot = √(Earth.μ / p) * (((1 - cos(Δθ)) / p) - (1 / r) - (1 / r0)) * tan(Δθ / 2)
+    gdot = 1 - (r0 / p) * (1 - cos(Δθ))
+
+    # Calculate the new position and velocity
+    R1 = f * R + g * V
+    V1 = fdot * R + gdot * V
+
+    return R1, V1
+end
+
+
+"""
+Determine a satellite's new position and velocity given existing state and time of flight.
+
+# Arguments
+- `a`: semi-major axis, in km
+- `e`: eccentricity
+- `θ`: true anomaly, in degrees
+- `Δt`: time of flight, in minutes
+
+# Returns
+- `r1`: propagated position radius, in km
+- `v1`: propagated velocity magnitude, in km/s
+- `γ1`: propagated flight path angle, in degrees
+- `θ1`: propagated true anomaly, in degrees
+"""
+function tof2RV(a::Float64, e::Float64, θ::Float64, Δt::Float64)::Tuple{Float64,Float64,Float64,Float64}
+    # Convert Δt to seconds and θ to radians
+    Δt = Δt * 60
+    θ = θ * (π / 180)
+
+    # Compute the mean motion
+    n = √(Earth.μ / a^3)
+
+    # Compute the eccentric anomaly
+    E = 2 * atan(√((1 - e) / (1 + e)) * tan(θ / 2))
+
+    # Compute the mean anomaly
+    M = E - e * sin(E)
+
+    # Compute the propagated mean anomaly at t₁
+    M1 = M + n * Δt
+
+    # Use Newton's method to find a suitable E1 that satisfies Kepler's equation
+    E1 = M1 + e * sin(M1) + (e^2 / 2) * sin(2 * M1)
+    errfunc(E) = E - e * sin(E) - M1
+    ∇errfunc(E) = 1 - e * cos(E)
+    err = errfunc(E1)
+    while abs(err) > 1e-6
+        E1 = E1 - err / ∇errfunc(E1)
+        err = errfunc(E1)
+    end
+
+    # Compute the propagated true anomaly at t₁
+    θ1 = 2 * atan(√((1 + e) / (1 - e)) * tan(E1 / 2))
+
+    # Compute the propagated radius at t₁
+    r1 = a * (1 - e * cos(E1))
+
+    # Compute the propagated velocity at t₁
+    v1 = √(2 * Earth.μ / r1 - Earth.μ / a)
+
+    # Compute the propagated flight path angle at t₁
+    γ1 = atan((v1^2 / Earth.μ) * (1 + e * cos(θ1)) / (1 - e^2))
+
+    # Convert to degrees
+    θ1 = θ1 * (180 / π)
+    γ1 = γ1 * (180 / π)
+
+    return r1, v1, γ1, θ1
+end
+
+export Earth, getapses, RV2koe, koe2rv, RV2coe, coe2RV, makeR, gibbs, singleangle, θ2E, E2θ, TOF, propagate, tof2RV
 
 end # module
